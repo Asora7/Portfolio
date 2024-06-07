88d6ea98bcbf9bd6d87d42341224f698a6349c25
Asora7 <verstr01474@stud.noroff.no>
2024-06-07 13:42:48 +0200

    project details

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 const projectCards = document.querySelectorAll('.project-card');
 
-// Add click event listener to each card
 projectCards.forEach(card => {
     card.addEventListener('click', () => {
-        // Get the project ID from the data attribute
+
         const projectId = card.getAttribute('data-project-id');
-        // Redirect to the corresponding detail page
+
         switch (projectId) {
             case '1':
                 window.location.href = `cards/museum.html`;
